490f4b67a64a691e6ad2cad741e466721b355db7
Michael Buch <michaelbuch12@gmail.com>
2018-05-22 13:42:58 +0100

    Keymap support

--- components/keymap.c
+++ components/keymap.c
@@ -1,13 +1,38 @@
 /* See LICENSE file for copyright and license details. */
-#include <dirent.h>
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
+#include <X11/XKBlib.h> 
+#include <X11/Xlib.h>
 
 #include "../util.h"
+#include <stdio.h>
 
 const char *
 keymap(void)
 {
-	return "keymap:";
+	Display *dpy;
+	if (!(dpy = XOpenDisplay(NULL))) {
+		warn("XOpenDisplay: Failed to open display");
+		return NULL;
+	}
+
+	XkbStateRec state;
+	if (XkbGetState(dpy, XkbUseCoreKbd, &state) != 0) {
+		warn("XkbGetState: failed to get keyboard state");
+		return NULL;
+	};
+
+	XkbDescRec* desc = XkbGetMap(dpy, XkbNamesMask, XkbUseCoreKbd);
+	if (!desc)
+		warn("XkbGetMap: failed to get XkbNamesMask keyboard component");
+
+	XkbGetNames(dpy, XkbSymbolsNameMask, desc);
+	if (desc->names) {
+		char *atom = XGetAtomName(dpy, desc->names->symbols);
+		XkbFreeClientMap(desc, XkbNamesMask, 1);
+		return atom;
+	} else {
+		warn("XkbGetNames: failed to retrieve symbols for keys");
+	}
+	
+	XkbFreeClientMap(desc, XkbNamesMask, 1);
+	return NULL;
 }
